29bc8fe5dbbace3803827e219c524195f9b1aa3a
Markel <bymarkel23@gmail.com>
2018-06-08 20:41:47 +0200

    Final release

--- Code/funtzioak.h
+++ Code/funtzioak.h
@@ -1,5 +1,5 @@
-#ifndef FUNTZ_H
-#define FUNTZ_H
+#ifndef FUNTZIOAK_H
+#define FUNTZIOAK_H
 
 #define MAX_KAR 128
 #define MAX_SOUNDS 15
@@ -8,9 +8,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <time.h>
-#include <SDL2/SDL_mixer.h>
-#include <SDL2/SDL.h>
+#include <sys/time.h>
+#include <wiringPi.h>
 #include <unistd.h>
 
 //noten soinuen helbidea
@@ -22,46 +21,56 @@
 #define LA ".//Soinuak//la"
 #define SI ".//Soinuak//si"
 //notak
-#define Do 97
-#define Re 115
-#define Mi 100
-#define Fa 102
-#define Sol 103
-#define La 104
-#define Si 106
-#define BUKATU 112
+#define Do 1
+#define Re 2
+#define Mi 3
+#define Fa 4
+#define Sol 5
+#define La 6
+#define Si 7
+#define Bukatu 8
+
+typedef struct egoera
+{
+	int egoera1;
+	int egoera2;
+	int egoera3;
+	int egoera4;
+	int egoera5;
+	int egoera6;
+	int egoera7;
+	int egoeraGen;
+
+}EGOERA;
 
 typedef struct Katea
 {
 	int tecla;
-	clock_t tartea;
+	long tartea;
 	struct Katea* ptrHurrengoa;
 }KATEA;
 
+void pinInit();
 char menua();
 int instrumento();
 void aukeraExekutatu(char aukera, KATEA ** burua, int instrumentua);
 void moduLibrea(int instrumentua);
-int notaEskatu();
-int erreproduzitu(int instrumentua, int nota);
+EGOERA zeinNota(int *nota, EGOERA egoera, int instrumentua);
+EGOERA notaHartu(int *nota, EGOERA egoera, int instrumentua);
+EGOERA nullJarri(EGOERA egoera);
+void forkFuntzioa( int instrumentua,int nota);
 void abestiaezabatu(KATEA **burua);
-KATEA* sortuNota(int tecla, clock_t tartea);
-void zerrendanGorde(int tecla, KATEA**burua, clock_t tartea);
+void game(int instrumentua);
+int notaJokua(int *nota, int egoera);
+
+//
+
+KATEA* sortuNota(int tecla, long tartea);
+void zerrendanGorde(int tecla, KATEA**burua, long tartea);
 int filtro(int tecla);
 void zerrendaBete(int instrumentua, KATEA**burua);
 void abestiaFitxategianIdatzi(KATEA *burua);
 void grabatutakoAbestiaIrakurri(KATEA **burua);
 void grabatutakoaErreproduzitu(KATEA *burua, int instrumentua);
-
-void audioInit();
-int loadSound(char *fileName);
-int loadTheMusic(char *fileName);
-int playSound(int idSound,int channel);
-int playMusic(void);
-void toggleMusic(void);
-void musicUnload(void);
-void soundsUnload();
-void audioTerminate(void);
-void initSound();
 
 #endif
